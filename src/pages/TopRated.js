fb7b19cb1ca71eac70d0dec011638fc7c217a76e
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-03-06 01:58:36 +0600

    navbar setup done

---
+++ src/pages/TopRated.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopRated = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-semibold">Top Rated</h1>
+        </div>
+    );
+};
+
+export default TopRated;
